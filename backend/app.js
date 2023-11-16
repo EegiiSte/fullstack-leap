517005b59446ad4d726166cbeb3b7ff1e31c2185
Erdenebayar Tserendagva <eddy2022@erdenebayarsair.mynetworksettings.com>
2023-11-15 20:25:19 -0800

    initialized express app

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+// import {express} from 'express';
+const express = require("express");
+const app = express();
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("hello world! from backend app.js");
+});
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
+});
